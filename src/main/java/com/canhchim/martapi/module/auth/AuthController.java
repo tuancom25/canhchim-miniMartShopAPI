7ed5b135fe1476620fc0b977e54f4e89eb84d540
trung-phung <trungphunng@gmail.com>
2022-06-04 16:47:21 +0700

    Test
    - Xoa out

--- src/main/java/com/canhchim/martapi/module/auth/AuthController.java
+++ src/main/java/com/canhchim/martapi/module/auth/AuthController.java
@@ -22,18 +22,14 @@ import java.util.Date;
 public class AuthController {
     private final IAuthService authService;
 
-    public AuthController(IAuthService authService) {
-        this.authService = authService;
-    }
+   
 
     @PostMapping("/admin/login")
     public ResponseEntity<?> adminLogin(@RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request) {
         try {
             LoginResponseDto loginResponseDto = authService.loginAdmin(loginRequestDto.getUsername(), loginRequestDto.getPassword());
             return ResponseEntity.status(HttpServletResponse.SC_OK)
-                    .header("Authorization", "Bearer " + loginResponseDto.getAccessToken())
-                    .body(loginResponseDto);
-        }
+                    .h
         catch (Exception e) {
             System.err.println(e);
             ErrorResponseDto errorResponseDto = new ErrorResponseDto();
